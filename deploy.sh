b20eea497980563470928207276e7391783fc4de
Tu Huynh <tuhuynh@Tus-MacBook-Pro.local>
2018-08-23 11:41:40 +0700

    Update

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ if [ -f  tmp/pids/server.pid  ]; then
  cat tmp/pids/server.pid | xargs kill 
 fi
 export SECRET_KEY_BASE=eee61b8de3fe3db85d6efb5c6bb849db6e9735f2f289787d79b932aebddda7ba525ab1cde5d8154ebd65da6286006dace56f72245bbd8e6c426418bcc906f35c
-RAILS_ENV=production rails s -p 3205  >/dev/null &
+RAILS_ENV=production rails s -p 4000 &
